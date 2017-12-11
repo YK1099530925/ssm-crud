1dfefb0b5ca23b8d02d45ec92f85e765ef4c2523
YK1099530925 <1099530925@qq.com>
2017-12-11 21:03:08 +0800

    ..

--- src/main/java/com/yk/crud/controller/EmployeeController.java
+++ src/main/java/com/yk/crud/controller/EmployeeController.java
@@ -1,16 +1,26 @@
 package com.yk.crud.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.yk.crud.bean.Employee;
+import com.yk.crud.service.EmployeeService;
 
 @Controller
 public class EmployeeController {
 
+	@Autowired
+	EmployeeService employeeService;
+	
 	/**
 	 * 查询员工数据（分页查询）
 	 * */
 	@RequestMapping(value="/emps")
 	public String getEmps() {
+		List<Employee> emps = employeeService.getAll();
 		return "list";
 	}
 }
--- src/main/java/com/yk/crud/service/EmployeeService.java
+++ src/main/java/com/yk/crud/service/EmployeeService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.yk.crud.bean.Employee;
 import com.yk.crud.dao.EmployeeMapper;
@@ -14,7 +15,11 @@ public class EmployeeService {
 	@Autowired
 	EmployeeMapper employeeMapper;
 	
-	public List<Employee> getAll() {
+	/**
+	 * 查询所有员工
+	 * @return
+	 */
+	public List<Employee> getAll(@RequestParam(value="pageNumber",defaultValue="1")Integer pageNumber) {
 		return employeeMapper.selectByExampleWithDept(null);
 	}
 
